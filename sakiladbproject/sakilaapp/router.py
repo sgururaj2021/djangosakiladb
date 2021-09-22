0b906e13f10a1d55bf345ca071647c7e11862188
sgururaj2021 <gururaj1931@gmail.com>
2021-09-22 18:40:18 +0530

    updated the router.py

--- sakiladbproject/sakilaapp/router.py
+++ sakiladbproject/sakilaapp/router.py
@@ -3,11 +3,12 @@ class SakilaRouter:
     A router to control all database operations on models in the
     auth and contenttypes applications.
     """
-    route_app_labels = {'sakilaapp', 'contenttypes'}
+    route_app_labels = {'sakilaapp',}
+
 
     def db_for_read(self, model, **hints):
         """
-        Attempts to read auth and contenttypes models go to auth_db.
+        Attempts to read sakilaapp models go to sakiladb.
         """
         if model._meta.app_label in self.route_app_labels:
             return 'sakiladb'
@@ -15,7 +16,7 @@ class SakilaRouter:
 
     def db_for_write(self, model, **hints):
         """
-        Attempts to write auth and contenttypes models go to auth_db.
+        Attempts to write sakilaapp models go to sakiladb.
         """
         if model._meta.app_label in self.route_app_labels:
             return 'sakiladb'
@@ -23,7 +24,7 @@ class SakilaRouter:
 
     def allow_relation(self, obj1, obj2, **hints):
         """
-        Allow relations if a model in the auth or contenttypes apps is
+        Allow relations if a model in the sakilaapp app is
         involved.
         """
         if (
@@ -35,8 +36,8 @@ class SakilaRouter:
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         """
-        Make sure the auth and contenttypes apps only appear in the
-        'auth_db' database.
+        Make sure the sakila app only appear in the
+        'sakiladb' database.
         """
         if app_label in self.route_app_labels:
             return db == 'sakiladb'
